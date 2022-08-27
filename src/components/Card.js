b4d620001ee9f7343169fdc8832ab166ebf8f209
Goga <gogaimerlishvili1@gmail.com>
2022-08-27 21:17:10 +0400

    get info from api

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,28 @@
 import React from "react";
 import Book from "../assets/images/book.jpg";
-const Card = () => {
+const Card = ({ book }) => {
+  console.log(book);
   return (
     <>
-      <div className="card">
-        <img src={Book} alt="" />
-        <div className="bottom">
-          <h3 className="title">React Js</h3>
-          <p className="amount">$100</p>
-        </div>
-      </div>
+      {book.map((item) => {
+        let thumbnail =
+          item.volumeInfo.imageLinks &&
+          item.volumeInfo.imageLinks.smallThumbnail;
+        let amount = item.saleInfo && item.saleInfo.saleability;
+        if (thumbnail !== undefined) {
+          return (
+            <>
+              <div className="card">
+                <img src={thumbnail} alt="" />
+                <div className="bottom">
+                  <h3 className="title">{item.volumeInfo.title}</h3>
+                  <p className="amount">${amount}</p>
+                </div>
+              </div>
+            </>
+          );
+        }
+      })}
     </>
   );
 };
