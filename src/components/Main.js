ff3bf8034f090536e3fc1ffe5b8ee292913cf7eb
Goga <gogaimerlishvili1@gmail.com>
2022-08-27 21:17:48 +0400

    get info from api with axios

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Books from "../assets/images/bg2.png";
 import { FcSearch } from "react-icons/fc";
 import Card from "./Card";
+import axios from "axios";
 
 const Main = () => {
+  const [search, setSearch] = useState("");
+  const [bookData, setData] = useState([]);
+
+  const searchBook = (evt) => {
+    if (evt.key === "Enter") {
+      axios
+        .get(
+          "https://www.googleapis.com/books/v1/volumes?q=" +
+            search +
+            "&key=AIzaSyCEJ8yaK1x3deUSaKj33CC56Jz7xgYWtm4" +
+            "&maxResults=40"
+        )
+        .then((res) => setData(res.data.items))
+        .catch((err) => console.log(err.message));
+    }
+  };
   return (
     <>
       <div className="header">
@@ -17,23 +34,20 @@ const Main = () => {
         <div className="row2">
           <h2>Find Your Book</h2>
           <div className="search">
-            <input type="text" placeholder="Enter Your Book Name" />
+            <input
+              type="text"
+              placeholder="Enter Your Book Name"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              onKeyPress={searchBook}
+            />
             <button>{<FcSearch />}</button>
           </div>
           <img src={Books} alt="books" />
         </div>
       </div>
 
-      <div className="container">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+      <div className="container">{<Card book={bookData} />}</div>
     </>
   );
 };
